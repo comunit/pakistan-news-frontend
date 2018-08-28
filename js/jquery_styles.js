3ccb45665d4d497fb4ff723e8b2eac1e4b904f78
Imran Rafique <comunit76@gmail.com>
2018-08-28 23:30:32 +0100

    last updated

--- js/jquery_styles.js
+++ js/jquery_styles.js
@@ -1,13 +1,17 @@
 	var transform_styles = ['-webkit-transform', '-ms-transform', 'transform'];
-	var minutesUpdated = $(".minutes h1").text();
 
-	var rotation = minutesUpdated * 9;
-	var fill_rotation = rotation;
-	var fix_rotation = rotation * 2;
-	for(i in transform_styles) {
-		$('.circle .fill, .circle .mask.full').css(transform_styles[i], 'rotate(' + fill_rotation + 'deg)');
-		$('.circle .fill.fix').css(transform_styles[i], 'rotate(' + fix_rotation + 'deg)');
-	}
+	$.get("https://pakstan-new-api.herokuapp.com/api/time", function(data){
+		var initparseDate = new Date(data);
+		var initUpdatedMinutes = moment().diff(initparseDate, 'minutes');
+		var rotation = initUpdatedMinutes * 9;
+		var fill_rotation = rotation;
+		var fix_rotation = rotation * 2;
+		$(".minutes h1").html(initUpdatedMinutes);
+		for(i in transform_styles) {
+			$('.circle .fill, .circle .mask.full').css(transform_styles[i], 'rotate(' + fill_rotation + 'deg)');
+			$('.circle .fill.fix').css(transform_styles[i], 'rotate(' + fix_rotation + 'deg)');
+		}
+	})
 
 	setInterval(() => {
 			$.get("https://pakstan-new-api.herokuapp.com/api/time", function(data){

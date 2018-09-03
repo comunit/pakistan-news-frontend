97367383381c5fa7613f7895b73891077068154f
Imran Rafique <comunit76@gmail.com>
2018-09-03 20:17:09 +0100

    testing

--- js/jquery_styles.js
+++ js/jquery_styles.js
@@ -22,7 +22,7 @@ $.get("https://pakstan-new-api.herokuapp.com/api/time", function(data) {
 setInterval(() => {
   $.get("https://pakstan-new-api.herokuapp.com/api/time", function(data) {
     var time = moment.utc(data, "ddd MMM D YYYY HH:mm:ss ZZ");
-    var updatedMinutes = moment().diff(time, "minutes");
+    var updatedMinutes = moment().diff(time, "mm");
     $(".minutes h1").html(updatedMinutes);
     for (i in transform_styles) {
       $(".circle .fill, .circle .mask.full").css(
